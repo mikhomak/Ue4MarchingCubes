8bac1382d9ac1c9256021b15a6ae49218c1e5f25
Misha <homakhaker@gmail.com>
2021-01-01 00:56:31 +0100

    Copied box provider

---
+++ MarchingCubes/Source/MarchingCubes/MarchingCubes/public/RuntimeMeshProviderMCubes.h
@@ -0,0 +1,40 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RuntimeMeshProvider.h"
+#include "URuntimeMeshProviderMCubes.generated.h"
+
+UCLASS(HideCategories = Object, BlueprintType)
+class URuntimeMeshProviderMCubes : public URuntimeMeshProvider
+{
+	GENERATED_BODY()
+private:
+	mutable FCriticalSection PropertySyncRoot;
+
+	UPROPERTY(Category = "RuntimeMesh|Providers|Box", VisibleAnywhere, BlueprintGetter=GetBoxRadius, BlueprintSetter=SetBoxRadius)
+	FVector BoxRadius;
+
+	UPROPERTY(Category = "RuntimeMesh|Providers|Box", VisibleAnywhere, BlueprintGetter=GetBoxMaterial, BlueprintSetter=SetBoxMaterial)
+	UMaterialInterface* Material;
+public:
+	UFUNCTION(Category = "RuntimeMesh|Providers|Box", BlueprintCallable)
+	FVector GetBoxRadius() const;
+	UFUNCTION(Category = "RuntimeMesh|Providers|Box", BlueprintCallable)
+	void SetBoxRadius(const FVector& InRadius);
+
+	UFUNCTION(Category = "RuntimeMesh|Providers|Box", BlueprintCallable)
+	UMaterialInterface* GetBoxMaterial() const;
+	UFUNCTION(Category = "RuntimeMesh|Providers|Box", BlueprintCallable)
+	void SetBoxMaterial(UMaterialInterface* InMaterial);
+
+
+protected:
+	void Initialize() override;
+	FBoxSphereBounds GetBounds() override;
+	bool GetSectionMeshForLOD(int32 LODIndex, int32 SectionId, FRuntimeMeshRenderableMeshData& MeshData) override;
+	FRuntimeMeshCollisionSettings GetCollisionSettings() override;
+	bool HasCollisionMesh() override;
+	bool GetCollisionMesh(FRuntimeMeshCollisionData& CollisionData) override;
+	bool IsThreadSafe() override;
+};
